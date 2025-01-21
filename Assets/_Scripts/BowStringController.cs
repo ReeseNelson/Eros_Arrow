b3ada96beffe321d95e79a60aca7a823ef786ab3
ReeseNelson <reesenelson125@gmail.com>
2025-01-20 17:46:02 -0700

    Bow pulling back using line Render

---
+++ Assets/_Scripts/BowStringController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class BowStringController : MonoBehaviour
+{
+    [SerializeField]
+    private String bowStringRenderer;
+
+    private XRGrabInteractable interactable;
+
+    [SerializeField]
+    private Transform midPointGrabObject;
+
+    private Transform interactor;
+
+    private void Awake()
+    {
+        interactable = midPointGrabObject.GetComponent<XRGrabInteractable>();
+    }
+
+    private void Start()
+    {
+        interactable.selectEntered.AddListener(PrepareBowString);
+        interactable.selectExited.AddListener(ResetBowString);
+    }
+
+    private void PrepareBowString(SelectEnterEventArgs arg0)
+    {
+        interactor = arg0.interactorObject.transform;
+    }
+
+    private void ResetBowString(SelectExitEventArgs arg0)
+    {
+        interactor = null;
+        midPointGrabObject.localPosition = Vector3.zero;
+        bowStringRenderer.CreateString(null); 
+    }
+
+    private void Update()
+    {
+        if (interactor != null)
+        {
+            bowStringRenderer.CreateString(midPointGrabObject.transform.position);
+        }
+    }
+}
