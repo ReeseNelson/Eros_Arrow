2a5abceda8a2733d4122d47ddcd56610529c43f2
ReeseNelson <reesenelson125@gmail.com>
2025-01-26 18:24:05 -0700

    Arrow shooting Behaviour

--- Assets/_Scripts/BowStringController.cs
+++ Assets/_Scripts/BowStringController.cs
@@ -1,20 +1,26 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using UnityEditor.Build.Content;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class BowStringController : MonoBehaviour
 {
-    [SerializeField]
-    private String bowStringRenderer;
+    [SerializeField] private String bowStringRenderer;
 
     private XRGrabInteractable interactable;
 
     [SerializeField]
-    private Transform midPointGrabObject;
+    private Transform midPointGrabObject, MidPointVisual, MidPointParent;
 
+    [SerializeField] 
+    private float bowStringStretchLimit = 0.3f;
+        
     private Transform interactor;
+
+    private float strength;
+
+    public UnityEvent OnBowPulled;
+    public UnityEvent<float> OnBowReleased;
 
     private void Awake()
     {
@@ -30,12 +36,18 @@ public class BowStringController : MonoBehaviour
     private void PrepareBowString(SelectEnterEventArgs arg0)
     {
         interactor = arg0.interactorObject.transform;
+        OnBowPulled?.Invoke();
     }
 
     private void ResetBowString(SelectExitEventArgs arg0)
     {
+        OnBowReleased?.Invoke(strength);
+        strength = 0;
+        
+        
         interactor = null;
         midPointGrabObject.localPosition = Vector3.zero;
+        MidPointVisual.localPosition = Vector3.zero;
         bowStringRenderer.CreateString(null); 
     }
 
@@ -43,7 +55,50 @@ public class BowStringController : MonoBehaviour
     {
         if (interactor != null)
         {
-            bowStringRenderer.CreateString(midPointGrabObject.transform.position);
+            Vector3 midPointLocalSpace = MidPointParent.InverseTransformPoint(midPointGrabObject.position);
+            
+            float midPointLocalZAbs = Mathf.Abs(midPointLocalSpace.z);
+
+            HandleStringPushedBackToStart(midPointLocalSpace);
+            
+            HandleStringPulledBackTolimit(midPointLocalZAbs, midPointLocalSpace);
+            
+            HandlePullingString(midPointLocalZAbs, midPointLocalSpace);
+            
+            
+            bowStringRenderer.CreateString(MidPointVisual.position);
+        }
+    }
+
+    private void HandlePullingString(float midPointLocalZAbs, Vector3 midPointLocalSpace)
+    {
+        if (midPointLocalSpace.z < 0 && midPointLocalZAbs < bowStringStretchLimit)
+        {
+            strength = Remap(midPointLocalZAbs, 0, bowStringStretchLimit, 0, 1);
+            MidPointVisual.localPosition = new Vector3(0, 0, midPointLocalSpace.z);
+        }
+    }
+
+    private float Remap(float value, int fromMin, float fromMax, int toMin, int toMax)
+    {
+        return (value - fromMin) / (fromMax - fromMin) * (toMax - toMin) + toMin;
+    }
+
+    private void HandleStringPulledBackTolimit(float midPointLocalZAbs, Vector3 midPointLocalSpace)
+    {
+        if (midPointLocalSpace.z < 0 && midPointLocalZAbs >= bowStringStretchLimit)
+        {
+            strength = 1;
+            MidPointVisual.localPosition = new Vector3(0, 0, -bowStringStretchLimit);
+        }
+    }
+
+    private void HandleStringPushedBackToStart(Vector3 midPointLocalSpace)
+    {
+        if (midPointLocalSpace.z >= 0)
+        {
+            strength = 0;
+            MidPointVisual.localPosition = Vector3.zero; 
         }
     }
 }
